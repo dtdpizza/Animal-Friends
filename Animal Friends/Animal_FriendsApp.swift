95f849d134671991ca35e653f975497a5d168cae
dtdpizza <80867965+dtdpizza@users.noreply.github.com>
2022-06-22 12:05:31 -0700

    Initial Commit

---
+++ Animal Friends/Animal_FriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animal_FriendsApp.swift
+//  Animal Friends
+//
+//  Created by Daron on 6/22/22.
+//
+
+import SwiftUI
+
+@main
+struct Animal_FriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
